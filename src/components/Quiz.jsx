d583033c1ad32c669ebc67ebc01f69241bf96d7c
Puc3k <kacperplocienniczak20@gmail.com>
2024-10-29 22:30:40 +0100

    fix: question timer cleanup, use key to reset component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png'
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
@@ -8,11 +10,13 @@ export default function Quiz() {
 
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers(prevUserAnswers => {
             return [...prevUserAnswers, selectedAnswer]
         })
-    }
+    }, [])
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
     if (quizIsComplete) {
         return <div id="summary">
@@ -27,6 +31,10 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer
+                    key={activeQuestionIndex}
+                    timeout={10000}
+                    onTimeout={handleSkipAnswer}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => (
